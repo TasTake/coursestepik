c6469de2e645bafe755a563948a7a5fe503dc7fa
TasTake <tasov98@gmail.com>
2022-11-05 18:45:25 +0600

    Lesson 4.2 6/12 Complete

--- lesson17/test_main_page.py
+++ lesson17/test_main_page.py
@@ -1,3 +1,4 @@
+from .pages.main_page import MainPage
 from asyncio.windows_events import NULL
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -6,8 +7,9 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open() 
+    page.should_be_login_link()                     # открываем страницу
+    #page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
     #use "pytest -v --tb=line --language=en test_main_page.py" to launch
